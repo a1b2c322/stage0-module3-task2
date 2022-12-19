503f56c69018cb3fe82abacdd119e683fc6e9eda
arastepa <stepanyan2298@gmail.com>
2022-12-19 22:04:52 +0400

    ..

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -6,9 +6,9 @@ public class NamingConvention {
         int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEFIVE = 5;
-        int VariableSix = 6;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        int variableSix = 6;
         int variableSeven = 7;
         int VariableEight = 8;
     }
